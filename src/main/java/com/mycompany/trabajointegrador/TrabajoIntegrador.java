ec74beffdbeffb1aa0e2056e02c617fe00d317be
davidpereyra625 <davidpereyra2013.dp@gmail.com>
2023-04-26 12:13:25 -0300

    update

---
+++ src/main/java/com/mycompany/trabajointegrador/TrabajoIntegrador.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajointegrador;
+
+/**
+ *
+ * @author david
+ */
+public class TrabajoIntegrador {
+
+    public static void main(String[] args) {
+        Equipo equipo = new Equipo();
+        equipo.mostrarEquipoSeleccionado(2);
+        System.out.println("-------------------------------------------");
+        equipo.mostrarTodosEquipos();
+    }
+}
